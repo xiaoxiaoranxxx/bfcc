c48e98a3cd3947af3d13a78027621dd87a2db6df
xiaoxiaoran <2425940130@qq.com>
2021-04-24 16:01:06 +0800

    生成xml文档

--- xiaoxiaoran5/php/1.php
+++ xiaoxiaoran5/php/1.php
@@ -2,4 +2,20 @@
 // DOM document obiect model
 // 每一种语言都有访问dom的方法
 
-
+$books_array = array(
+    'php1' => 'xiuxiu1',
+    'php2' => 'xiuxiu2',
+    'php3' => 'xiuxiu3',
+    'php5' => 'xiuxiu5'
+);
+$doc = new DOMDocument('1.0', 'utf-8');  //创建
+$doc->formatOutput = true;  //格式化输出
+$books = $doc->createElement('books');  //创建根
+$doc->appendChild($books);  //创建子节点
+foreach ($books_array as $k => $v) {
+    $book = $doc->createElement('book');
+    $name = $doc->createElement('name', $k);
+    $books->appendChild($book);
+    $book->appendChild($name);
+}
+$doc->save('./xiaoxiaoran5/php/test1.xml');
