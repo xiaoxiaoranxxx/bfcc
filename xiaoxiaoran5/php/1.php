84818dfc7d272f432cbe245bd9cda51d4ad4bc24
xiaoxiaoran <2425940130@qq.com>
2021-04-24 15:44:39 +0800

    xx

---
+++ xiaoxiaoran5/php/1.php
@@ -0,0 +1,5 @@
+<?php
+// DOM document obiect model
+// 每一种语言都有访问dom的方法
+
+
--- xiaoxiaoran5/xml/3.php
+++
@@ -1,8 +0,0 @@
-<?php
-// DOM document obiect model
-// 每一种语言都有访问dom的方法
-$doc = new DOMDocument();
-$doc->load('3.xml');
-
-$names = $doc->getElementsByTagName('books');
-var_dump($names);
