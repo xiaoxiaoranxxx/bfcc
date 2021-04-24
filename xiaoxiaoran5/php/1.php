2e0c9f723b2a2ac54aa1f8d104792d81bef48959
xiaoxiaoran <2425940130@qq.com>
2021-04-24 16:08:21 +0800

    设置type

--- xiaoxiaoran5/php/1.php
+++ xiaoxiaoran5/php/1.php
@@ -8,12 +8,13 @@ $books_array = array(
     'php3' => 'xiuxiu3',
     'php5' => 'xiuxiu5'
 );
-$doc = new DOMDocument('1.0', 'utf-8');  //创建
+$doc = new DOMDocument('1.0', 'utf-8');  //创建文档节点
 $doc->formatOutput = true;  //格式化输出
-$books = $doc->createElement('books');  //创建根
+$books = $doc->createElement('books');  //创建元素节点
 $doc->appendChild($books);  //创建子节点
 foreach ($books_array as $k => $v) {
     $book = $doc->createElement('book');
+    $book->setAttribute('type',$v);
     $name = $doc->createElement('name', $k);
     $books->appendChild($book);
     $book->appendChild($name);
