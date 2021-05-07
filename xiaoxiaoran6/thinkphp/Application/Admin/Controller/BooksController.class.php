6b1596f21b3d4fba447968eeba4a3d42b20faf58
xiaoxiaoran <2425940130@qq.com>
2021-05-07 16:37:34 +0800

    快速实例化

--- xiaoxiaoran6/thinkphp/Application/Admin/Controller/BooksController.class.php
+++ xiaoxiaoran6/thinkphp/Application/Admin/Controller/BooksController.class.php
@@ -10,7 +10,17 @@ class BooksController extends Controller  //声明控制器类并继承父类
     // 127.0.0.1\xiaoxiaoran\bfcc\xiaoxiaoran6\thinkphp\index.php\Admin\Books\shilihua
     public function shilihua()
     {
-        $model = new \Admin\Model\BooksModel();
-        // dump($model); 模型在实例化是就关联了这张表
+        // $model = new \Admin\Model\BooksModel(); //普通的实例化
+        // dump($model); 模型在实例化就自动关联了这张表
+
+        $model = D('Books');  //D实例化自己创建的模型,和普通的是一样的
+        //$model = D();  //直接实例化父类的模型
+
+        //$model = M('Books'); //关联数据表
+        //$model = M(); //实例化父类的模型,不关联数据表
+
+        //父类不能满足需求就D
+
+        dump($model);
     }
 }
