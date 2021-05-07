6861726257a542bfd478f29ba1a0ff8ec9de372a
xiaoxiaoran <2425940130@qq.com>
2021-05-07 15:22:39 +0800

    循环遍历

--- xiaoxiaoran6/thinkphp/Application/Runtime/Cache/Home/5c992bb3029117cfc00764f10e17c6e2.php
+++ xiaoxiaoran6/thinkphp/Application/Runtime/Cache/Home/5c992bb3029117cfc00764f10e17c6e2.php
@@ -12,6 +12,17 @@
 
     <hr>
     <?php echo ($array2["0"]["0"]); ?>
+    <hr>volist <br>
+    <?php if(is_array($array)): $i = 0; $__LIST__ = $array;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vol): $mod = ($i % 2 );++$i; echo ($vol); ?>-<?php endforeach; endif; else: echo "" ;endif; ?>
+
+    <?php if(is_array($array)): foreach($array as $key=>$for): echo ($for); ?>-<?php endforeach; endif; ?>
+
+    volist支持多样化<br>
+
+    <?php if(is_array($array2)): $i = 0; $__LIST__ = $array2;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vol): $mod = ($i % 2 );++$i; if(is_array($vol)): $i = 0; $__LIST__ = $vol;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i; echo ($vo); ?>--<?php endforeach; endif; else: echo "" ;endif; ?>
+        <br><?php endforeach; endif; else: echo "" ;endif; ?>
+
+    <?php if(is_array($array2)): foreach($array2 as $key=>$for): echo ($for[0]); ?>-<?php echo ($for[1]); ?>-<?php echo ($for[2]); ?>-<?php echo ($for[3]); ?>-<br><?php endforeach; endif; ?>
 
 </body>
 </html>
