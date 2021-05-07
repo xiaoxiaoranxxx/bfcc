35cc9f4728650bd5c030d8eaa59154e365ad067a
xiaoxiaoran <2425940130@qq.com>
2021-05-07 22:42:58 +0800

    数据库的增删改查

--- xiaoxiaoran6/thinkphp/Application/Admin/Controller/BooksController.class.php
+++ xiaoxiaoran6/thinkphp/Application/Admin/Controller/BooksController.class.php
@@ -23,4 +23,69 @@ class BooksController extends Controller  //声明控制器类并继承父类
 
         dump($model);
     }
+
+    //CURD
+
+    public function tianjia()
+    {
+        $model = M('Books');
+        $data = array(
+            "bName" => 'xiao',
+            "bTypeId" => 6,
+            "publishing" => 'xiaoxiaoran',
+            "price" => 33,
+            "pubDate" => '2010-02-22',
+            "author" => 'xiu',
+            "ISBN" => '34434'
+        );
+        $data1 = array(
+            "bName" => 'xiao1',
+            "bTypeId" => 61,
+            "publishing" => 'xiaoxiaoran1',
+            "price" => 313,
+            "pubDate" => '2010-02-22',
+            "author" => 'xiu1',
+            "ISBN" => '344314'
+        );
+        $data2 = array($data, $data1);
+        $result = $model->add($data);  //返回新增的主键id
+        // $result = $model->addAll($data2); //批量添加
+        dump($result);
+    }
+    public function xiugai()
+    {
+        $model = M('Books');
+        $data = array(
+            "bId" => '45',
+            "publishing" => 'xiaoxiaoran6666',
+            "price" => 315553,
+        );
+        $result = $model->save($data);
+        dump($result); //没有主键会错误,返回受到影响的行数
+    }
+    public function chaxun()
+    {
+        $model = M('Books');
+        // $model->select() 查全部
+        // $model->select(id) 
+        // $model->select('id1','id2',...)
+
+        // $model->find() 查表中的第一条记录
+        // $model->find(id)  
+
+        // select返回二维数组
+        $data = $model->select();
+        $data = $model->select(13);
+        $data = $model->select('2,12');
+        $data = $model->find();
+        $data = $model->find(2);
+
+        dump($data);
+    }
+    public function shanchu()
+    {
+        $model = M('Books');
+        $result = $model->delete(47);//没有主键会错误,返回受到影响的行数
+        dump($result);
+    }
 }
